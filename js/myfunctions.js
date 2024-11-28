94d1cfe1fe1df7f3c9737eec8fa79525931156b1
Ophek Alon <ophekal@std.hac.ac.il>
2024-11-28 16:26:57 +0200

    validators

--- js/myfunctions.js
+++ js/myfunctions.js
@@ -3,28 +3,18 @@
 document.addEventListener('DOMContentLoaded', function(){
 
     document.getElementById("add-task-main").addEventListener('click', createAndEditTaskModule.create);
-    InitDom.initCategoryOptions();
-    InitDom.InitPriorityOptions();
+    InitDomModule.initCategoryOptions();
+    InitDomModule.InitPriorityOptions();
+    document.querySelector('button[type="submit"]').addEventListener('click', createAndEditTaskModule.submit);
+    document.getElementById('cancelBtn').addEventListener('click', createAndEditTaskModule.cancel);
 
 });
 
-const InitDom = (function(){
+// ----------------------- const ---------------------------------------
+const form = document.getElementById("form-row");
 
-    /**
-     *
-     */
 
-    const InitdatePickers = () => {
-        const datePickers = document.querySelectorAll('[data-coreui-toggle="date-picker"]');
-        datePickers.forEach(picker => {
-            new coreui.DatePicker(picker, {
-                locale: picker.getAttribute('data-coreui-locale') || 'en-US',
-                timepicker: picker.getAttribute('data-coreui-timepicker') === 'true',
-                date: picker.getAttribute('data-coreui-date') || null
-            });
-        });
-    };
-
+const InitDomModule = (function(){
 
     /**
      *
@@ -68,7 +58,6 @@ const InitDom = (function(){
 
 
     return{
-        InitdatePickers,
         InitPriorityOptions,
         initCategoryOptions
     };
@@ -84,7 +73,7 @@ const UiModule = (function() {
      */
     const toggleFormVisibility = (showForm) => {
         const mainPage= document.getElementsByClassName("main-page-section")[0];
-        const form = document.getElementById("form-row");
+        //const form = document.getElementById("form-row");
 
         if(showForm) {
             mainPage.classList.add("d-none");
@@ -117,12 +106,14 @@ const UiModule = (function() {
     /**
      *
      */
-    const renderFormError = () => {
+    const renderFormErrors = () => {
 
     };
 
     return {
-        toggleFormVisibility
+        toggleFormVisibility,
+        renderTaskList,
+        renderFormErrors
     }
 })();
 
@@ -131,23 +122,35 @@ const taskDataModule = (function (){
 
     let taskList = [];      // inside the module without access to the outside
 
+    /**
+     * This function adds the task into the task list
+     * @param task
+     */
     const addTask = (task) => {
         taskList.push(task);
     };
 
+    /**
+     * This function receives a task index and deletes that task from the list
+     * @param index
+     */
     const deleteTask = (index) => {
         taskList.splice(index, 1);
     };
 
     /**
-     *
+     * This function receives a task index and sends it back
      * @param index
-     * @returns {*}
+     * @returns taskList[index]
      */
     const getTask = (index) => {
         return taskList[index];
     };
 
+    /**
+     * This function retrieves the task list
+     * @returns *[]
+     */
     const getTasks = () => {
         return taskList;    // return a copy to avoid direct change
     };
@@ -167,15 +170,26 @@ const createAndEditTaskModule = ( function () {
      * @param event
      */
     const createTask = function (event) {
-
         UiModule.toggleFormVisibility(true);
     }
 
     /**
-     *
+     * This function is responsible for submitting a task.
+     * First, it extracts the form's data, then it calls on a function that checks if the fields are valid.
+     * If the inputs are valid it adds the task to the list and resets the form, else it'll show the errors.
      */
     const submitTask = function () {
+        const formData = formModule.getFormData();
 
+        if (validationTaskModule.isValid()) {
+            taskDataModule.addTask(formData);
+            formModule.clearForm();
+            UiModule.toggleFormVisibility(false);
+            UiModule.renderTaskList();
+        }
+        else {
+            UiModule.renderFormErrors();
+        }
     };
 
     /**
@@ -199,11 +213,14 @@ const createAndEditTaskModule = ( function () {
      * @param taskIndex
      */
     const cancelTask = function (taskIndex) {
-
+        formModule.clearForm();
+        UiModule.toggleFormVisibility(false);
     }
 
     return {
-        create: createTask
+        create: createTask,
+        submit: submitTask,
+        cancel: cancelTask
     }
 })();
 
@@ -214,27 +231,24 @@ const formModule = (function () {
      *
      */
     const getFormData = () => {
-
+        return {name: document.getElementById("taskName").value,
+                category: document.getElementById("category").value,
+                priority: document.getElementById("priority").value,
+                date: document.getElementById("datetimepicker1").value,
+                description: document.getElementById("description").value};
     };
-
-    /**
-     * go over all the form and check if valid
-     * @param taskData
-     */
-    const isValid = (taskData) => {
-
-    };
-
 
     /**
      *
      */
     const clearForm = () => {
+        document.getElementById('taskForm').reset();
 
     };
 
     return {
-
+        clearForm,
+        getFormData
     }
 })();
 
@@ -246,7 +260,13 @@ const validationTaskModule = ( function () {
      * @param element
      */
     const validTaskName = function ( element ) {
+        const nameRegex = /^[A-Za-z0-9\s]+$/;
 
+        // checking if the name is unique
+        let listOfLists =taskDataModule.getTasks();
+        const exists = listOfLists.some(subList => subList.includes(element));
+
+        return !exists && nameRegex.test(element);
     }
 
     /**
@@ -254,7 +274,7 @@ const validationTaskModule = ( function () {
      * @param element
      */
     const validCategory = function ( element ) {
-
+        return (element === 'Select Category');
     }
 
     /**
@@ -262,7 +282,7 @@ const validationTaskModule = ( function () {
      * @param element
      */
     const validPriority = function ( element ) {
-
+        return element;  // element is empty if priority not checked
     }
 
     /**
@@ -278,10 +298,39 @@ const validationTaskModule = ( function () {
      * @param element
      */
     const validDescription = function ( element ) {
-
+        const descriptionRegex = /^[A-Za-z0-9 .,!?'"-]*$/;
     }
 
+    // Map form field to their validator functions
+    const fieldValidators = {
+        name: validTaskName,
+        category: validCategory,
+        priority: validPriority,
+        dueDateTime: validDueDateTime,
+        description: validDescription
+    };
+
+    /**
+     * This function receives the data that's been filled in the form.
+     * It goes over all the fields and check if the input is valid by calling the
+     * appropriate validator function
+     *
+     * @param taskData
+     */
+    const isValid = (taskData) => {
+        for (const [field, value] of Object.entries(taskData)) {
+            const validator = fieldValidators[field];
+            if (validator && !validator(value)) {
+                return false; // Return false on first validation failure
+            }
+        }
+        return true; // All fields valid
+    };
+
+
+
     return {
+        isValid,
         validTaskName,
         validCategory,
         validPriority,
